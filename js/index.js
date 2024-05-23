2c151906d8e887f88194e17ed8b4c8be8b2b16ba
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-05-23 14:49:51 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,148 @@
+// Game Constants & Variables
+let inputDir = { x: 0, y: 0 };
+const foodSound = new Audio("music/food.mp3");
+const gameOverSound = new Audio("music/gameover.mp3");
+let speed = 6;
+let score = 0;
+let lastPaintTime = 0;
+let snakeArr = [{ x: 13, y: 15 }];
+
+food = { x: 6, y: 7 };
+
+function main(ctime) {
+  window.requestAnimationFrame(main);
+  // console.log(ctime)
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
+    return;
+  }
+  lastPaintTime = ctime;
+  gameEngine();
+}
+
+function isCollide(snake) {
+  // Hàm xử lý nếu đâm vào chính mình
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true;
+    }
+  }
+  // Xử lý nếu đâm vào tường
+  if (
+    snake[0].x >= 18 ||
+    snake[0].x <= 0 ||
+    snake[0].y >= 18 ||
+    snake[0].y <= 0
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function gameEngine() {
+  //Tăng độ dài
+  if (isCollide(snakeArr)) {
+    gameOverSound.play();
+    inputDir = { x: 0, y: 0 };
+    document.getElementById("gameMessage").innerHTML = "<b>Game Over.</b>";
+    snakeArr = [{ x: 13, y: 15 }];
+    score = 0;
+    speed = 6; // Reset speed snake nếu thua
+  }
+
+  // Nếu đã ăn food, hãy tăng score, reset thức ăn và tăng speed
+  if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    foodSound.play();
+    score += 1;
+    if (score > hiscoreval) {
+      hiscoreval = score;
+      localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+      hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
+    }
+    scoreBox.innerHTML = "Score: " + score;
+    snakeArr.unshift({
+      x: snakeArr[0].x + inputDir.x,
+      y: snakeArr[0].y + inputDir.y,
+    });
+    let a = 2;
+    let b = 16;
+    food = {
+      x: Math.round(a + (b - a) * Math.random()),
+      y: Math.round(a + (b - a) * Math.random()),
+    };
+
+    // Tăng speed lên 1 đơn vị
+    speed += 1;
+  }
+
+  // Moving the snake
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] };
+  }
+
+  snakeArr[0].x += inputDir.x;
+  snakeArr[0].y += inputDir.y;
+
+  // Hiển thị rắn
+  board.innerHTML = "";
+  snakeArr.forEach((e, index) => {
+    snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = e.y;
+    snakeElement.style.gridColumnStart = e.x;
+
+    if (index === 0) {
+      snakeElement.classList.add("head");
+    } else {
+      snakeElement.classList.add("snake");
+    }
+    board.appendChild(snakeElement);
+  });
+  // Hiển thị apple
+  foodElement = document.createElement("i");
+  foodElement.style.gridRowStart = food.y;
+  foodElement.style.gridColumnStart = food.x;
+  foodElement.classList.add("food", "fa-brands", "fa-apple", "fa-2xl");
+  board.appendChild(foodElement);
+}
+
+let hiscore = localStorage.getItem("hiscore");
+if (hiscore === null) {
+  hiscoreval = 0;
+  localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+} else {
+  hiscoreval = JSON.parse(hiscore);
+  hiscoreBox.innerHTML = "Highest score: " + hiscore;
+}
+
+window.requestAnimationFrame(main);
+window.addEventListener("keydown", (e) => {
+  document.getElementById("gameMessage").innerHTML = "";
+  inputDir = { x: 0, y: 1 }; // Start the game
+  switch (e.key) {
+    case "ArrowUp":
+      console.log("ArrowUp");
+      inputDir.x = 0;
+      inputDir.y = -1;
+      break;
+
+    case "ArrowDown":
+      console.log("ArrowDown");
+      inputDir.x = 0;
+      inputDir.y = 1;
+      break;
+
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      inputDir.x = -1;
+      inputDir.y = 0;
+      break;
+
+    case "ArrowRight":
+      console.log("ArrowRight");
+      inputDir.x = 1;
+      inputDir.y = 0;
+      break;
+    default:
+      break;
+  }
+});
